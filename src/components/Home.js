051fc3c762bc0d5360399858f92fa7749157a233
HLNatraj <161406266+HLNatraj@users.noreply.github.com>
2024-07-25 01:44:48 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,7 +14,8 @@ const Home = () => {
             alt="Profile"
             className="w-24 h-24 md:w-32 md:h-32 lg:w-60 lg:h-60 rounded-full border-4 border-white bg-[url('profile1.jpeg')] bg-cover"
           />
-          <h1 className='text-black text-center text-2xl font-bold mt-4 md:mt-0 pl-16'>NATRAJ</h1>
+                  <h1 className="text-black text-center text-2xl font-bold mt-4 md:mt-0 px-4 md:px-8 lg:px-16">NATRAJ</h1>
+
 
         </div>
         <div className="relative text-black max-w-2xl mt-6 md:mt-0 md:ml-6">
